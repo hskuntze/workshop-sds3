3a4f8ba678dc50dfc6d8168a64a662312f95fb8d
Hassan <hskuntze@gmail.com>
2021-09-06 14:15:35 -0300

    Componentes básicos inseridos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/hskuntze" target="_blank" rel="noreferrer">Hassan Kuntze</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Instagram para contato: <a href="https://instagram.com/hskuntze" target="_blank" rel="noreferrer">@hskuntze</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
