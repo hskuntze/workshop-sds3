8c9bffce8d8a8fe3a29db6ec0fd0f8b79816462d
Hassan <hskuntze@gmail.com>
2021-09-10 19:16:50 -0300

    Camadas do backend, Paginação

---
+++ backend/src/main/java/com/kuntzeprojects/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.kuntzeprojects.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kuntzeprojects.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/kuntzeprojects/dsvendas/services/SaleService.java
@@ -0,0 +1,40 @@
+package com.kuntzeprojects.dsvendas.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kuntzeprojects.dsvendas.dto.SaleDTO;
+import com.kuntzeprojects.dsvendas.entities.Sale;
+import com.kuntzeprojects.dsvendas.repositories.SaleRepository;
+import com.kuntzeprojects.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repo;
+	
+	@Autowired
+	private SellerRepository sellerRepo;
+	
+	/**
+	 * 		Adicionando a classe  na busca do B.D. (findAll)
+	 * automaticamente habilitamos a paginação (10 em 10, 20 em 20 etc.) 
+	 * 					nos resultados do objeto
+	 */
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		/**
+		 * Criamos esta consulta de Sellers para que fique armazenado na memória
+		 * cache, não necessitando da consulta feita automaticamente pelo JPA ao
+		 * 					retornar o Seller de cada venda
+		 */
+		sellerRepo.findAll(); 
+		Page<Sale> res = repo.findAll(pageable);
+		return res.map(e -> new SaleDTO(e));
+	}
+}
